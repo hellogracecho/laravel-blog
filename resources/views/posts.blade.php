960eacc3f772fa9dcf97ad5216297d79fb309320
Grace Cho <grace@splitmango.com>
2021-05-18 10:27:23 -0700

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">First Hello world</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore omnis maiores aperiam eveniet amet voluptatum necessitatibus molestiae inventore non repellat possimus voluptates vero illum, quasi, consequatur eligendi temporibus dignissimos at autem! Voluptates ducimus inventore harum quas voluptatum neque nam veritatis.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">Second Hello world</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore omnis maiores aperiam eveniet amet voluptatum necessitatibus molestiae inventore non repellat possimus voluptates vero illum, quasi, consequatur eligendi temporibus dignissimos at autem! Voluptates ducimus inventore harum quas voluptatum neque nam veritatis.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">Third Hello world</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore omnis maiores aperiam eveniet amet voluptatum necessitatibus molestiae inventore non repellat possimus voluptates vero illum, quasi, consequatur eligendi temporibus dignissimos at autem! Voluptates ducimus inventore harum quas voluptatum neque nam veritatis.</p>
+    </article>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+// ** Wildcard {}
+Route::get('posts/{post}', function ($slug) {
+    // Find a post by its slug and pass it to a view called "post"
+
+    // Validate if the path exist
+    if ( ! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html") ) {
+        // return redirect('/');
+        abort(404);
+    }
+
+    // ** Use Caching  with unique key + how long to cache (in sec) + function
+    // helper function for time now()->addMinutes(20) 
+    // arrow function: fn() => file_get_contents($path)
+    $post = cache()->remember("post.{$slug}", 5, function() use($path) {
+        return file_get_contents($path);
+    });
+
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z_\-]+');
