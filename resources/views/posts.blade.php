0c601d0d7f224c43673d907ed88675ab0784e06e
Grace Cho <grace@splitmango.com>
2021-06-04 16:09:21 -0700

    CSS work in progress

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -16,15 +16,9 @@
     @include ('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        <x-post-featured-card :post="$posts[0]"/>
     
         <div class="lg:grid lg:grid-cols-2">      
-            <x-post-card />
-            <x-post-card />
-        </div>
-    
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
             <x-post-card />
             <x-post-card />
         </div>
