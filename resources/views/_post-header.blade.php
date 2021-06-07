5a701cba458348e1ba6a3099ffdc43e2d817eae7
Grace Cho <grace@splitmango.com>
2021-06-07 12:06:09 -0700

    Icon blade with name prop

--- resources/views/_post-header.blade.php
+++ resources/views/_post-header.blade.php
@@ -19,15 +19,7 @@
                 <x-slot name="trigger">
                     <button class="py-2 px-3 w-full lg:w-32 flex lg:inline-flex text-sm text-left font-semibold inline-flex">
                         {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
-                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                        height="22" viewBox="0 0 22 22">
-                            <g fill="none" fill-rule="evenodd">
-                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                </path>
-                                <path fill="#222"
-                                        d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                            </g>
-                        </svg>
+                        <x-icon name="down-arrow" class="absolute pointer-events-none"/>
                     </button>
                 </x-slot>
                 <!-- default slot down below -->
